c1c31e9d1bcf86929692929e15cb8cbfe3c0e335
Jeff Rafter <jeffrafter@gmail.com>
2019-06-02 09:01:21 -0700

    Configuring Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,86 @@
+'use strict'
+
+module.exports = {
+  siteMetadata: {
+    title: 'Example',
+    description: 'This is an example site I made.',
+    siteUrl: 'https://example.com',
+    author: {
+      name: 'Jeff Rafter',
+      url: 'https://twitter.com/jeffrafter',
+      email: 'jeffrafter@gmail.com',
+    },
+    social: {
+      twitter: 'https://twitter.com/jeffrafter',
+      github: 'https://github.com/jeffrafter',
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1280,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://jeffrafter.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jeff Rafter`,
+        short_name: `jeffrafter.com`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/logo.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-sharp`,
+  ],
+}
